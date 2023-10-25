4598d6f1dbca269278709f9f8906e8f35e664d2e
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-10-25 00:52:07 -0300

    feat: add checkout module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { CheckoutModule } from './modules/checkout/checkout.module';
 
 @Module({
-  imports: [],
+  imports: [CheckoutModule],
   controllers: [AppController],
   providers: [],
 })
